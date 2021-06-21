103e29732504e4d718024467779e49b2a7697288
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-06-21 10:42:13 +0100

    Use updated Plausible Analytics scripts

--- docs/javascripts/extra.js
+++
@@ -1,8 +0,0 @@
-if (document.location.hostname === "docs.opensafely.org") {
-  var script = document.createElement("script");
-  script.async = true;
-  script.defer = true;
-  script.setAttribute("data-domain", "docs.opensafely.org");
-  script.src = "https://pa.opensafely.org/js/index.js";
-  document.head.appendChild(script);
-}
