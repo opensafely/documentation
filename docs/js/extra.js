7a5f83e6400910bf46bc8d1b5af3cff0fad2cc97
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2021-06-21 12:36:14 +0100

    Simplify analytics script

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,21 +1,21 @@
 if (document.location.hostname === "docs.opensafely.org") {
-  var script = document.createElement("script");
-  script.defer = true;
-  script.setAttribute("data-domain", "docs.opensafely.org");
-  script.setAttribute("data-api", "/pa/api/event");
-  script.src = "/pa/js/script.js";
-  document.head.appendChild(script);
-
-  // Serve legacy compat script to IE11 users
   var ua = window.navigator.userAgent;
   var trident = ua.indexOf("Trident/");
   var msie = ua.indexOf("MSIE ");
+
+  var script = document.createElement("script");
+  script.defer = true;
+
+  // Serve legacy compat script to IE users
   if (trident > 0 || msie > 0) {
-    var script = document.createElement("script");
-    script.defer = true;
     script.id = "plausible";
     script.setAttribute("data-domain", "docs.opensafely.org");
     script.src = "https://plausible.io/js/plausible.compat.js";
-    document.head.appendChild(script);
+  } else {
+    script.setAttribute("data-domain", "docs.opensafely.org");
+    script.setAttribute("data-api", "/pa/api/event");
+    script.src = "/pa/js/script.js";
   }
+
+  document.head.appendChild(script);
 }
