df9ff5a4bfffe7e00d3b6f3ca2502c4002381ee6
George Hickman <george@ghickman.co.uk>
2022-02-14 15:22:13 +0000

    comment out databuilder DSL examples until the new DSL is in

--- databuilder/snippets/dsl.py
+++ databuilder/snippets/dsl.py
@@ -1,24 +1,26 @@
 # :snippet imports
-from databuilder.dsl import Cohort
-from databuilder.tables import clinical_events, practice_registrations
+# TODO: replace this when the new DSL is in
+# from databuilder.dsl import Cohort
+# from databuilder.tables import clinical_events, practice_registrations
 
 
 # :endsnippet
 
 # :snippet dataset-definition
-cohort = Cohort()
+# TODO: replace this when the new DSL is in
+# cohort = Cohort()
 
-index_date = "2020-11-01"
-registered = (
-    practice_registrations.filter(practice_registrations.date_start <= index_date)
-    .filter(practice_registrations.date_end >= index_date)
-    .exists_for_patient()
-)
-cohort.set_population(registered)
+# index_date = "2020-11-01"
+# registered = (
+#     practice_registrations.filter(practice_registrations.date_start <= index_date)
+#     .filter(practice_registrations.date_end >= index_date)
+#     .exists_for_patient()
+# )
+# cohort.set_population(registered)
 
-cohort.code = (
-    clinical_events.sort_by(clinical_events.date)
-    .first_for_patient()
-    .select_column(clinical_events.code)
-)
+# cohort.code = (
+#     clinical_events.sort_by(clinical_events.date)
+#     .first_for_patient()
+#     .select_column(clinical_events.code)
+# )
 # :endsnippet
