57fd6212b14c67dfc4c9d8be378508bcf42657f2
Steven Maude <git@stevenmaude.co.uk>
2022-08-18 17:35:22 +0100

    Change to using prescriptions table
    It is slightly simpler than clinical events.

--- databuilder/ehrql-tutorial-examples/2a_multiple_dataset_definition.py
+++ databuilder/ehrql-tutorial-examples/2a_multiple_dataset_definition.py
@@ -1,5 +1,5 @@
 from databuilder.query_language import Dataset
-from databuilder.tables import clinical_events, patients
+from databuilder.tables import patients, prescriptions
 
 
 dataset = Dataset()
@@ -8,6 +8,8 @@ year_of_birth = patients.date_of_birth.year
 dataset.set_population(year_of_birth >= 2000)
 
 dataset.sex = patients.sex
-dataset.first_code = (
-    clinical_events.sort_by((clinical_events.code)).first_for_patient().code
+dataset.most_recent_dmd_code = (
+    prescriptions.sort_by(prescriptions.processing_date)
+    .last_for_patient()
+    .prescribed_dmd_code
 )
