0104fa9fc846a401f197fb5cacc3b00b3ebc277a
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2024-03-15 10:30:23 +0000

    Run Prettier and ESLint

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,7 +1,7 @@
-var domain = "docs.opensafely.org";
+const domain = "docs.opensafely.org";
 
 if (document.location.hostname === domain) {
-  var script = document.createElement("script");
+  const script = document.createElement("script");
   script.defer = true;
   script.setAttribute("data-domain", domain);
   script.id = "plausible";
@@ -10,13 +10,19 @@ if (document.location.hostname === domain) {
   document.head.appendChild(script);
 }
 
-document.addEventListener("DOMContentLoaded", function () { patchCopyCodeButtons(); });
+document.addEventListener("DOMContentLoaded", () => {
+  patchCopyCodeButtons();
+});
 
 function patchCopyCodeButtons() {
   // select all "copy" buttons whose target selector is a <code> element
-  codeCopyButtons = document.querySelectorAll('button.md-clipboard[data-clipboard-target$="code"]');
-  for (let btn of codeCopyButtons) {
-    let codeTextToCopy = getTextWithoutPromptAndOutput(btn.dataset.clipboardTarget);
+  codeCopyButtons = document.querySelectorAll(
+    'button.md-clipboard[data-clipboard-target$="code"]',
+  );
+  for (const btn of codeCopyButtons) {
+    const codeTextToCopy = getTextWithoutPromptAndOutput(
+      btn.dataset.clipboardTarget,
+    );
     btn.dataset.clipboardText = codeTextToCopy;
   }
 }
@@ -25,12 +31,14 @@ function getTextWithoutPromptAndOutput(targetSelector) {
   const targetElement = document.querySelector(targetSelector);
 
   // exclude "Generic Prompt" and "Generic Output" spans from copy
-  const excludedClasses = ['gp', 'go'];
+  const excludedClasses = ["gp", "go"];
 
-  let text = '';
+  let text = "";
   for (const node of targetElement.childNodes) {
-    if (node.nodeType == Node.TEXT_NODE |
-      (node.nodeType == Node.ELEMENT_NODE && !excludedClasses.includes(node.className))
+    if (
+      (node.nodeType == Node.TEXT_NODE) |
+      (node.nodeType == Node.ELEMENT_NODE &&
+        !excludedClasses.includes(node.className))
     ) {
       text += node.textContent;
     }
